a8970148520865fccb041dbc615c0576d434db5c
hamdi4-beep <hamdiakippuden@gmail.com>
2025-07-11 18:18:43 +0100

    used the react error boundary library

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { ErrorBoundary } from 'react-error-boundary'
 import CommentSection from './components/CommentSection'
 import { store } from './store'
 import {Provider} from 'react-redux'
+import FallbackRender from './components/ErrorComponent'
 
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
-        <CommentSection />
-      </Provider>
+      <ErrorBoundary fallbackRender={FallbackRender}>
+        <Provider store={store}>
+          <CommentSection />
+        </Provider>
+      </ErrorBoundary>
     </div>
   )
 }
--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -1,23 +1,20 @@
 import { commentCreated } from "../features/comments/CommentsSlice";
 import { useAppDispatch } from "../hooks";
 import CommentsList from "./CommentsList";
-import ErrorBoundary from "./ErrorBoundary";
 import FormComponent from "./FormComponent";
 
 function CommentSection() {
     const dispatch = useAppDispatch()
 
     return (
-        <ErrorBoundary>
-            <div className="comment-section">
-                <CommentsList />
+        <div className="comment-section">
+            <CommentsList />
 
-                <FormComponent
-                    placeholderValue='Add a comment...'
-                    dispatchHandler={(content: string) => dispatch(commentCreated(content))}
-                />
-            </div>
-        </ErrorBoundary>
+            <FormComponent
+                placeholderValue='Add a comment...'
+                dispatchHandler={(content: string) => dispatch(commentCreated(content))}
+            />
+        </div>
     )
 }
 
