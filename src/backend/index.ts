36162e3d5914b2af31fc87986ae1b153a5a78e89
hamdi4-beep <hamdiakippuden@gmail.com>
2025-08-21 19:49:55 +0100

    fixed a bug where the file was not being sent on consequetive requests

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,22 +1,17 @@
 import express from 'express'
 import { createReadStream } from 'fs'
 
-const fs = createReadStream('../data/comments.json')
-
 const app = express()
 app.listen(3000, () => console.log('Listening for requests on', 3000))
 
 app.get('/comments', (request, response) => {
     response.writeHead(200, {
         'access-control-allow-origin': '*',
-        'transfer-encoding': 'chunked'
+        'content-type': 'application/json'
     })
     
-    fs
+    createReadStream('../data/comments.json')
         .pipe(response)
-        .on('error', err => {
-            response
-                .status(500)
-                .end('Something went wrong!')
-        })
+        .on('error', console.error)
+        .on('finish', () => console.log('Finished streaming the file'))
 })
