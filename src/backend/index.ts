7403cc450223695e455ea6542a2461578b5ed75d
hamdi4-beep <hamdiakippuden@gmail.com>
2025-08-22 13:45:15 +0100

    added some basic error handling for the backend

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,16 +1,26 @@
 import express from 'express'
-import { createReadStream } from 'fs'
+import { createReadStream, existsSync } from 'fs'
 
 const app = express()
 app.listen(3000, () => console.log('Listening for requests on', 3000))
 
 app.get('/comments', (request, response) => {
+    const path = '../data/commen.json'
+
+    if (!existsSync(path)) {
+        response
+            .status(500)
+            .end('Something went wrong!\n')
+
+        return
+    }
+
     response.writeHead(200, {
         'access-control-allow-origin': '*',
         'content-type': 'application/json'
     })
     
-    createReadStream('../data/comments.json')
+    createReadStream(path)
         .pipe(response)
         .on('error', console.error)
         .on('finish', () => console.log('Finished streaming the file'))
