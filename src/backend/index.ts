3a5efa59fb307828bfa258a8dd075375644d5a02
hamdi4-beep <hamdiakippuden@gmail.com>
2025-08-22 14:33:05 +0100

    added an endpoint for each comment with an id

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -5,7 +5,7 @@ const app = express()
 app.listen(3000, () => console.log('Listening for requests on', 3000))
 
 app.get('/comments', (request, response) => {
-    const path = '../data/commen.json'
+    const path = '../data/comments.json'
 
     if (!existsSync(path)) {
         response
@@ -14,14 +14,44 @@ app.get('/comments', (request, response) => {
 
         return
     }
-
-    response.writeHead(200, {
-        'access-control-allow-origin': '*',
-        'content-type': 'application/json'
-    })
     
     createReadStream(path)
         .pipe(response)
         .on('error', console.error)
         .on('finish', () => console.log('Finished streaming the file'))
+})
+
+app.get('/comments/:id', (request, response) => {
+    const id = request.params['id']
+    let body = ''
+
+    const readStream = createReadStream('../data/comments.json')
+
+    readStream
+        .on('data', chunk => body += chunk)
+        .on('end', () => {
+            body = JSON.parse(body)
+
+            const data = (body as unknown as {
+                'byId': {
+                    [key: string]: Object
+                }
+            })
+
+            if (!data.byId[id]) {
+                response
+                    .status(500)
+                    .json({
+                        message: `A comment with an ID of ${id} doesn't exists`
+                    })
+                    .end()
+
+                return
+            }
+
+            response
+                .status(200)
+                .json(data['byId'][id])
+                .end()
+        })
 })
