31d1687ed163c30e64ad791ec619d2544cf82773
hamdi4-beep <hamdiakippuden@gmail.com>
2025-08-29 12:20:12 +0100

    removed utils folder because it's unused

--- src/utils/util.ts
+++
@@ -1,8 +0,0 @@
-export const fetchData = async (URI: string) => {
-    try {
-        const response = await fetch(URI)
-        return await response.json()
-    } catch (err) {
-        console.error(err)
-    }
-}
