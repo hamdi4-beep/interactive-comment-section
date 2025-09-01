807250d96246188c6c540d7602abc7cded2f1208
hamdi4-beep <hamdiakippuden@gmail.com>
2025-09-01 13:04:19 +0100

    added an Entity to easily reference either a comment or reply type

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,14 @@
 import type { CommentState, UserComment, CommentID } from "@/features/comments/types";
 import type { UserReply } from "@/features/replies/types";
 
-export const incrementScore = (entity: UserComment | UserReply, currentScore: number) =>
+type Entity = UserComment | UserReply
+
+export const incrementScore = (entity: Entity, currentScore: number) =>
     entity.score = currentScore === entity.score ?
         // Updates the score by two to skip the current score because upvoting is only meant to increase the current value by one
         entity.score + 1 : entity.score < currentScore ? entity.score + 2 : currentScore
 
-export const decrementScore = (entity: UserComment | UserReply, currentScore: number) =>
+export const decrementScore = (entity: Entity, currentScore: number) =>
     entity.score = currentScore === entity.score ?
         // Updates the score by two to skip the current score because upvoting is only meant to decrease the current value by one
         entity.score - 1 : entity.score > currentScore ? entity.score - 2 : currentScore
