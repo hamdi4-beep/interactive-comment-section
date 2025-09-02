cd17f03b551e82fd8e8bf67b09a40b0dc7cbcb97
hamdi4-beep <hamdiakippuden@gmail.com>
2025-09-02 09:59:27 +0100

    updated a mistyped comment in the reply component

--- src/features/replies/Reply.tsx
+++ src/features/replies/Reply.tsx
@@ -19,7 +19,7 @@ const Reply = React.memo(function Reply({
 
     React.useEffect(() => {
         return () => {
-            // prevents the score from being resetted if the reply object was deleted entirely other resets the score if the component was just unmounted.
+            // prevents the score from being resetted if the reply object was deleted entirely otherwise reset the score if the component was just unmounted.
             if (reply) replyScoreReseted({ id })
         }
     }, [id])
