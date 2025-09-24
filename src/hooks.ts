090e4f950d62187bed453258a77303addace45ee
Hamdi Kamel <hamdiakippuden@gmail.com>
2025-09-24 07:17:08 +0100

    used immer instead of deep cloning the state object

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,70 +1,93 @@
-import { useReducer } from "react"
+import { useCallback, useReducer } from "react"
 import { reducer } from "./reducer"
 import data from './data/comments.json'
 import { v4 as uuidv4 } from 'uuid'
-import { CreateComment, CreateReply, DeleteComment, EditComment, UpdateScore } from "./context"
+import { Comment, CommentId, CreateComment, CreateReply, DeleteComment, EditComment, UpdateScore } from "./context"
+import { useImmerReducer } from "use-immer"
 
 /*
     Just keeping this here for reference. I'm going to define some "abstract" methods that update state so I don't have to track dispatch calls throughout the codebase. The easier it gets to modify the way dispatch calls work, the less time I can spend on making several changes within components that shouldn't care how state gets updated.
 */
+export type State = {
+    byId: Record<CommentId, Comment>
+    allId: CommentId[]
+}
 
 export function useComments() {
-    const [comments, dispatch] = useReducer(reducer, data)
+    const [comments, dispatch] = useImmerReducer(reducer, data)
 
-    const commentCreated: CreateComment = content =>
-        dispatch({
-            type: 'CREATE_COMMENT',
-            payload: {
-                content,
-                newId: uuidv4()
-            }
-        })
+    const commentCreated: CreateComment = useCallback(
+        content =>
+            dispatch({
+                type: 'CREATE_COMMENT',
+                payload: {
+                    content,
+                    newId: uuidv4()
+                }
+            }),
+        []
+    )
 
-    const replyCreated: CreateReply = (id, username, content) =>
-        dispatch({
-            type: 'CREATE_REPLY',
-            payload: {
-                id,
-                newId: uuidv4(),
-                username,
-                content
-            }
-        })
+    const replyCreated: CreateReply = useCallback(
+        (id, username, content) =>
+            dispatch({
+                type: 'CREATE_REPLY',
+                payload: {
+                    id,
+                    newId: uuidv4(),
+                    username,
+                    content
+                }
+            }),
+        []
+    )
 
-    const commentEdited: EditComment = (id, content) =>
-        dispatch({
-            type: 'EDIT_COMMENT',
-            payload: {
-                id,
-                content
-            }
-        })
+    const commentEdited: EditComment = useCallback(
+        (id, content) =>
+            dispatch({
+                type: 'EDIT_COMMENT',
+                payload: {
+                    id,
+                    content
+                }
+            }),
+        []
+    )
 
-    const commentDeleted: DeleteComment = id =>
-        dispatch({
-            type: 'DELETE_COMMENT',
-            payload: {
-                id
-            }
-        })
+    const commentDeleted: DeleteComment = useCallback(
+        id =>
+            dispatch({
+                type: 'DELETE_COMMENT',
+                payload: {
+                    id
+                }
+            }),
+        []
+    )
 
-    const scoreIncremented: UpdateScore = (id, currentScore) =>
-        dispatch({
-            type: 'INCREMENT_SCORE',
-            payload: {
-                id,
-                currentScore
-            }
-        })
+    const scoreIncremented: UpdateScore = useCallback(
+        (id, currentScore) =>
+            dispatch({
+                type: 'INCREMENT_SCORE',
+                payload: {
+                    id,
+                    currentScore
+                }
+            }),
+        []
+    )
 
-    const scoreDecremented: UpdateScore = (id, currentScore) =>
-        dispatch({
-            type: 'DECREMENT_SCORE',
-            payload: {
-                id,
-                currentScore
-            }
-        })
+    const scoreDecremented: UpdateScore = useCallback(
+        (id, currentScore) =>
+            dispatch({
+                type: 'DECREMENT_SCORE',
+                payload: {
+                    id,
+                    currentScore
+                }
+            }),
+        []
+    )
 
     return {
         comments,
