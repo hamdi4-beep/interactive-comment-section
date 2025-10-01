10c0137f8feeb7e522f56e54850ea5614eb78ffe
Hamdi Kamel <hamdiakippuden@gmail.com>
2025-10-01 21:38:46 +0100

    fix: update CSS import path in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import StateProvider from './context'
-import './index.css'
+import './css/index.css'
 
 createRoot(document.getElementById('root') as HTMLDivElement).render(
   <StrictMode>
