82bcff0c95c7db48aa592178347e64959103decb
Hamdi Kamel <hamdiakippuden@gmail.com>
2025-11-14 13:40:49 +0100

    feat: enhance ScoreComponent buttons with accessibility attributes

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -42,7 +42,7 @@ const ScoreComponent = ({
 
   return (
     <div className="score-component">
-      <button onClick={handleIncrementScoreClick}>
+      <button type='button' aria-label="upvote" aria-hidden="true" onClick={handleIncrementScoreClick}>
         <div className="icon-img">
           <img src="/images/icon-plus.svg" alt="" />
         </div>
@@ -50,7 +50,7 @@ const ScoreComponent = ({
 
       <span>{score}</span>
 
-      <button onClick={handleDecrementScoreClick}>
+      <button type='button' aria-label="downvote" aria-hidden="true" onClick={handleDecrementScoreClick}>
         <div className="icon-img">
           <img src="/images/icon-minus.svg" alt="" />
         </div>
